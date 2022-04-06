a800e940f55f2c74c7772dc0d2414d1621bfd524
Sleepy Z <88429202+zumm66@users.noreply.github.com>
2022-04-06 12:59:20 +0700

    update db

--- src/config/db/db.config.js
+++ src/config/db/db.config.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose');
-
 const mongoAtlasUri = "mongodb+srv://zumm66:Zxx-789520@cluster0.uakvi.mongodb.net/blog?retryWrites=true&w=majority";
-
-// try {
-//     // Connect to the MongoDB cluster
-//     mongoose.connect(
-//         mongoAtlasUri,
-//         { useNewUrlParser: true, useUnifiedTopology: true },
-//         () => console.log(" Mongoose blog mindx is connected")
-//     );
-// } catch (e) {
-//     console.log("could not connect");
-// }
 
 async function connect() {
 
