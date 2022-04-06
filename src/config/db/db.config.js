e2a99fbf33b9519679af05d6e8121281382612bc
Sleepy Z <88429202+zumm66@users.noreply.github.com>
2022-04-06 11:54:31 +0700

    update mongodb url

--- src/config/db/db.config.js
+++ src/config/db/db.config.js
@@ -1,17 +1,30 @@
 const mongoose = require('mongoose');
 
-async function connect() {
+const mongoAtlasUri = "mongodb+srv://zumm66:Zxx-789520@cluster0.uakvi.mongodb.net/blog?retryWrites=true&w=majority";
 
-    try {
-        await mongoose.connect('mongodb://localhost:27017/blog', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connected to Database!');
-    } catch (error) {
-        console.log('Connect failure!'); 
-    }
-
+try {
+    // Connect to the MongoDB cluster
+    mongoose.connect(
+        mongoAtlasUri,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log(" Mongoose blog mindx is connected")
+    );
+} catch (e) {
+    console.log("could not connect");
 }
+
+// async function connect() {
+
+//     try {
+//         await mongoose.connect('mongodb://localhost:27017/blog', {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log('Connected to Database!');
+//     } catch (error) {
+//         console.log('Connect failure!'); 
+//     }
+
+// }
 
 module.exports = { connect };
